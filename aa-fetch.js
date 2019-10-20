1de8e6639802f9ac85f74eb9b712bb6b1da761f3
Will Hough <will.hough@nike.com>
2019-10-19 20:08:20 -0700

    Update to version 1.1.0 Added generation of PDF files

--- aa-fetch.js
+++ aa-fetch.js
@@ -11,6 +11,7 @@ const package = require('./package.json')
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 const fs = require('fs');
+const pdf = require('html-pdf');
 
 const args = require('yargs')
   .usage("Usage: $0 --slug SLUG")
@@ -28,16 +29,34 @@ const args = require('yargs')
 
 const url = `https://www.audaud.com/${args.slug}/`
 
-const writeArticle = function(slug, article) {
+const writeHtml = function(slug, article) {
   return new Promise(resolve => {
-    fs.writeFile(`${slug}.html`, article, function(err) {
+    fs.writeFile(`html/${slug}.html`, article, 'utf-8', function(err) {
       err && console.log(err);
-      return err
+      resolve()
     })
   })
 }
 
+const writePdf = function(slug, article) {
+  return new Promise(resolve => {
+    pdf.create(article,{ format: 'Letter' }).toFile(`pdf/${slug}.pdf`, (err, res) => {
+      err && console.log(err);
+      resolve()
+    });
+  })
+}
+
+const JQSCRIPT = "<script type='text/javascript' src='https://www.audaud.com/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>"
+const PGBREAK = "<P style='page-break-after: auto'>"
+const slug = args.slug.trim()
+
+slug.length || process.exit(0)
+
 console.log(`Fetching: ${url}`)
+fs.mkdir('html', err => err)
+fs.mkdir('pdf', err => err)
+
 fetch(url)
     .then(res => res.text())
     .then(body => {
@@ -52,8 +71,10 @@ fetch(url)
       contentArea.find('div.sharedaddy').remove()
       contentArea.find('#jp-relatedposts').remove()
 
-      writeArticle(args.slug, contentArea.html())
-        .then(err => {
-          process.exit(0)
-        })
-      });
+      const articleHtml = `<body>${JQSCRIPT}${contentArea.html()}${PGBREAK}</body>`
+      const p1 = writeHtml(slug, articleHtml);
+      const p2 = writePdf(slug, articleHtml)
+      Promise.all([p1, p2]).then(res => {
+        process.exit(0)
+      })
+    });
